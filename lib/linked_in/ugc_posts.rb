789e6fe51d801aff43f0c0b6bbb2fb88637e0952
Andrea Fusco <fusco@extendi.it>
2022-01-12 17:42:48 +0100

    fix get_post 400 error on restli-v2

--- lib/linked_in/ugc_posts.rb
+++ lib/linked_in/ugc_posts.rb
@@ -10,13 +10,13 @@ module LinkedIn
     def get_post(options = {})
       urn = options.delete(:urn)
       path = "/ugcPosts/#{urn}?viewContext=AUTHOR"
-      ugc_posts_get(path, options)
+      ugc_posts_get(path, false, options)
     end
 
     def get_posts_batch(options = {})
       urns = options.delete(:urns).join(',')
       path = "/ugcPosts?ids=List(#{urns})"
-      ugc_posts_get(path, options)
+      ugc_posts_get(path, true, options)
     end
 
     # You can retrieve all UGC posts for a member or an organization. Use authors=List(person Urn) or
@@ -24,14 +24,14 @@ module LinkedIn
     def posts(options = {})
       urn = options.delete(:urn)
       path = "/ugcPosts?q=authors&authors=List(#{urn})"
-      ugc_posts_get(path, options)
+      ugc_posts_get(path, true, options)
     end
 
     private
 
-      def ugc_posts_get(path, options = {})
+      def ugc_posts_get(path, restli, options = {})
         options[:headers] ||= {}
-        options[:headers]['X-Restli-Protocol-Version'] = '2.0.0'
+        options[:headers]['X-Restli-Protocol-Version'] = '2.0.0' if restli
         get(path, options)
       end
 
